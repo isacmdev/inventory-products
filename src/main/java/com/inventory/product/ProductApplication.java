615f3c478c2b2d56bec798c2b3a1d2860a95eba1
isacmdev <devisa.coder@gmail.com>
2026-02-19 16:28:29 -0500

    redis

--- src/main/java/com/inventory/product/ProductApplication.java
+++ src/main/java/com/inventory/product/ProductApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
-@EnableCaching
+/*@EnableCaching*/
 public class ProductApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/inventory/product/application/ProductServiceUseCase.java
+++ src/main/java/com/inventory/product/application/ProductServiceUseCase.java
@@ -4,9 +4,9 @@ import com.inventory.product.domain.entity.Product;
 import com.inventory.product.domain.ports.ProductInterfacePortIn;
 import com.inventory.product.domain.ports.ProductInterfacePortOut;
 import lombok.AllArgsConstructor;
-import org.springframework.cache.CacheManager;
+/*import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Cacheable;*/
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +17,9 @@ import java.util.List;
 public class ProductServiceUseCase implements ProductInterfacePortIn {
 
     private final ProductInterfacePortOut productInterfacePortOut;
+/*
     private final CacheManager cacheManager;
+*/
 
     @Override
     public Product addProduct(Product product) {
@@ -26,7 +28,7 @@ public class ProductServiceUseCase implements ProductInterfacePortIn {
 
     @Override
     @Transactional
-    @CacheEvict(cacheNames = "productById", key = "#id")
+/*    @CacheEvict(cacheNames = "productById", key = "#id")*/
     public Product updateProduct(Long id, Product product) {
         Product existing = productInterfacePortOut.findById(id);
 
@@ -43,7 +45,9 @@ public class ProductServiceUseCase implements ProductInterfacePortIn {
     }
 
     @Override
+/*
     @Cacheable(cacheNames = "productById", key = "#id")
+*/
     public Product getProductById(Long id) {
         if(id <= 0) {
             throw new IllegalArgumentException("El id del producto no puede ser negativo");
@@ -52,23 +56,31 @@ public class ProductServiceUseCase implements ProductInterfacePortIn {
     }
 
     @Override
+/*
     @Cacheable(cacheNames = "productBySku", key = "#sku")
+*/
     public Product getProductBySku(String sku) {
         return productInterfacePortOut.findBySku(sku);
     }
 
 
     @Override
+/*
     @CacheEvict(cacheNames = "productById", key = "#id")
+*/
     public void deleteProduct(Long id) {
         if (id <= 0) {
             throw new IllegalArgumentException("El id del producto no puede ser negativo");
         }
         Product product = productInterfacePortOut.findById(id);
+/*
         String sku = product.getSku();
+*/
 
         productInterfacePortOut.deleteById(id);
+/*
         cacheManager.getCache("productBySku").evict(sku);
+*/
     }
 
     @Override
